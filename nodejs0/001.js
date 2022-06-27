edab6111b4994d58abe288f3afbd333d07ee7e24
jasonKim <kimyk96@gmail.com>
2022-06-27 16:51:53 +0900

    220627

---
+++ nodejs0/001.js
@@ -0,0 +1,115 @@
+// grr = {};
+// console.log(typeof grr);
+// console.log(grr)
+
+
+
+// const a = 17, b = 5;
+// // % 사용금지. a/b 몫 나머지 출력
+
+// console.log(a / b);
+// console.log(Math.floor(a / b));
+// console.log(a - (b * Math.floor(a / b)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 운전면허실기시험 : 수험 번호, 이름, 주소, 연락처, 점수,
+// // 자바의 타입 : 기본 ( 원시 ) 타입과 참조타입
+// // 기본타입은 값 1개를 저장, 참조타입은 객체를 가리키는 리모컨
+
+// const name = "홍길동";
+// const score = 70;
+
+// const obj = {
+//     name: "홍길동",
+//     score: 70,
+// }
+
+// obj.prototype.pass = function(){
+//     if( this.score > 60 ){ return true; }else{ return false };
+// }
+
+
+// console.log(typeof obj);
+// console.log(obj);
+// console.log(obj.score);
+// console.log(person.pass());
+
+// console.log(typeof NaN) // number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function drivingTest(idx, name, address, phone, score) {
+//     this.idx = idx;
+//     this.name = name;
+//     this.name = name;
+//     this.name = name;
+//     this.name = name;
+
+// }
